37c0d7aadb46fa20a8f388622563a7d8005866a5
skc3779 <skc3779@gmail.com>
2014-07-13 21:07:40 +0900

    기본구조 변경

---
+++ src/test/java/kangchun/hamcrest/HamcrestTest.java
@@ -0,0 +1,7 @@
+package kangchun.hamcrest;
+
+/**
+ * Created by kangchun on 2014-07-13.
+ */
+public class HamcrestTest {
+}
--- src/test/java/kangchun/mockito/MockitoTest.java
+++ src/test/java/kangchun/mockito/MockitoTest.java
@@ -1,4 +1,4 @@
-package org.mokito;
+package kangchun.mockito;
 
 import lombok.Data;
 import lombok.extern.slf4j.Slf4j;
